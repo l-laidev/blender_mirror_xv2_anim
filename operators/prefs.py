599fe430202ae9afa93053e3e1863c305da05e53
H.A. <114867851+l-laidev@users.noreply.github.com>
2026-03-24 00:28:13 +0500

    refactor + add separate option to deselect Optimize + add 2 modes (xv2, sz)

--- operators/prefs.py
+++ operators/prefs.py
@@ -1,15 +1,31 @@
-from bpy.props import EnumProperty
+import bpy
 
 
-settings = EnumProperty(
-    name="Mirror Options",
-    description="Enable/Disable what to mirror",
-    items=[
-        ("Location", "Location", "Mirror X Location"),
-        ("Rotation", "Rotation", "Mirror Bone Rotations"),
-        ("Scale", "Scale", "Mirror Bone Scaling"),
-        ("Optimize", "Optimize", "Good for sparsely keyframed animations. Might leave some frames if all frames are keyed.")
-    ],
-    default={"Location", "Rotation", "Scale", "Optimize"},
-    options={"ENUM_FLAG"},
-)
+class AddonProps(bpy.types.PropertyGroup):
+    mirroring_options: bpy.props.EnumProperty(
+        name="Mirror Options",
+        description="Enable/Disable what to mirror",
+        items=[
+            ("Location", "Location", "Mirror X Location"),
+            ("Rotation", "Rotation", "Mirror Bone Rotations"),
+            ("Scale", "Scale", "Mirror Bone Scaling"),
+        ],
+        default={"Location", "Rotation", "Scale"},
+        options={"ENUM_FLAG"},
+    )
+    
+    mirroring_mode: bpy.props.EnumProperty(
+        name="Preset",
+        description="Mirroring mode",
+        items=[
+            ("XV2", "XV2", "XV2"),
+            ("SZ", "SZ", "SparkingZERO"),
+        ],
+        default="XV2",
+    )
+
+    optimize_frame_selection: bpy.props.BoolProperty(
+        name="Optimize",
+        description="Good for sparsely keyframed animations. Might leave some frames if all frames are keyed.",
+        default=True,
+    )
