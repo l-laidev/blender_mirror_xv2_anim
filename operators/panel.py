2d16760a76305f002413852821e95bafe2dcab10
H.A. <114867851+l-laidev@users.noreply.github.com>
2026-03-23 20:29:46 +0500

    add custom panel

---
+++ operators/panel.py
@@ -0,0 +1,13 @@
+import bpy
+from . import MirrorAnim
+
+
+class AddonPanel(bpy.types.Panel):
+    bl_idname = "OBJECT_PT_xv2_mirror_anim"
+    bl_label = "Mirror XV2 Animation"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Mirror XV2"
+    
+    def draw(self, context):
+        self.layout.operator(MirrorAnim.bl_idname)
